27959e36443b3739b2a344a940db7324c8cd7a01
salwarmoghal <sulthanso73@gmail.com>
2024-12-13 12:02:29 +0530

    handles cors

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,15 +4,21 @@ import dotenv from "dotenv";
 import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
 import messageRoutes from "./routes/message.route.js"
+import cors from "cors";
 
 dotenv.config(); 
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true 
+}))
 
 const PORT = process.env.PORT || 5000
 app.use("/api/auth", authRoutes)
 app.use("/api/message", messageRoutes)
+
 
 app.listen(PORT, () => {
     console.log("Server running on :", PORT);
