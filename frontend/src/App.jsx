78bf2bfaf83f4f1aaa117a86715971dc4c5b4ac7
salwarmoghal <sulthanso73@gmail.com>
2024-12-13 17:40:03 +0530

    adds loader and protects our routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,5 @@
 import Navbar from "./components/Navbar";
-import { Route, Routes } from "react-router-dom";
-
+import { Route, Routes, Navigate } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import SignUpPage from "./pages/SignUpPage";
 import LoginPage from "./pages/LoginPage";
@@ -8,23 +7,31 @@ import SettingsPage from "./pages/SettingsPage";
 import ProfilePage from "./pages/ProfilePage";
 import { useAuthStore } from "./store/useAuthStore";
 import { useEffect } from "react";
-
+import { Loader } from "lucide-react";
 const App = () => {
-  const {authUser, checkAuth} = useAuthStore();
+  const { authUser, checkAuth, isCheckingAuth } = useAuthStore();
 
   useEffect(() => {
     checkAuth();
-  }, [checkAuth])
+  }, [checkAuth]);
+
+  if (isCheckingAuth && !authUser) {
+    return (
+      <div className="flex items-center justify-center h-screen">
+        <Loader className="size-10 animate-spin" />
+      </div>
+    );
+  }
 
   return (
     <div>
       <Navbar />
       <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/signup" element={<SignUpPage />} />
-        <Route path="/login" element={<LoginPage />} />
+        <Route path="/" element={ authUser ? <HomePage />: <Navigate to="/login" /> } />
+        <Route path="/signup" element={ authUser ?  <Navigate to="/" /> : <SignUpPage />} />
+        <Route path="/login" element={ authUser ?  <Navigate to="/" /> : <LoginPage />} />
         <Route path="/settings" element={<SettingsPage />} />
-        <Route path="/profile" element={<ProfilePage />} />
+        <Route path="/profile" element={ authUser ? <ProfilePage /> : <Navigate to="/login" /> } />
       </Routes>
     </div>
   );
